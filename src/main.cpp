56b291fe8b28c95c280d2c0d434c7ee6d64cc1fc
MaxBark1 <maxim.barkin@gmail.com>
2023-07-12 12:17:20 -0400

    change library dependancy and com port from 7 to 4

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,31 @@
+//2018.6.28
 #include <Arduino.h>
 #include "avr8-stub.h"
-#include "app_api.h" // only needed with flash breakpoints
-int i=0;
-void setup()
+#include <dht_nonblocking.h>
+
+#
+#define DHT_SENSOR_TYPE DHT_TYPE_11
+//#define DHT_SENSOR_TYPE DHT_TYPE_21
+//#define DHT_SENSOR_TYPE DHT_TYPE_22
+
+static const int DHT_SENSOR_PIN = 2;
+DHT_nonblocking dht_sensor( DHT_SENSOR_PIN, DHT_SENSOR_TYPE );
+
+void setup( )
 {
-pinMode(LED_BUILTIN, OUTPUT);
-// initialize the avr-debugger
-debug_init();
+  Serial.begin( 9600);
 }
-void loop()
+void loop( )
 {
-digitalWrite(LED_BUILTIN, HIGH);
-i++;
-delay(500);
-digitalWrite(LED_BUILTIN, LOW);
-delay(500);
+  float temperature;
+  float humidity;
+
+  if(dht_sensor.measure(&temperature, &humidity)){
+    Serial.print( "T = " );
+    Serial.print( temperature, 1 );
+    Serial.print( " deg. C, H = " );
+    Serial.print( humidity, 1 );
+    Serial.println( "%" );
+  }
 }
+
