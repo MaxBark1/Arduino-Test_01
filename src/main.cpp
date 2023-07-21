b2de419e6a46e278fe917099f02b651da8994b13
MaxBark1 <maxim.barkin@gmail.com>
2023-07-21 13:28:36 -0400

    changed serial print to debug message. changed pin from 2 to 13 dues limitations of arv_debug. tested breakpoint (breakpoint() function)

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,13 @@
 #include <Arduino.h>
 #include "avr8-stub.h"
 #include <dht_nonblocking.h>
-#dbarkin added the comment
-#
+#include <string.h>
+
 #define DHT_SENSOR_TYPE DHT_TYPE_11
 //#define DHT_SENSOR_TYPE DHT_TYPE_21
 //#define DHT_SENSOR_TYPE DHT_TYPE_22
 
-static const int DHT_SENSOR_PIN = 2;
+static const int DHT_SENSOR_PIN = 13;
 DHT_nonblocking dht_sensor( DHT_SENSOR_PIN, DHT_SENSOR_TYPE );
 
 void setup( )
@@ -19,13 +19,16 @@ void loop( )
 {
   float temperature;
   float humidity;
+  String myMessage;
 
   if(dht_sensor.measure(&temperature, &humidity)){
-    debug_message( "T =");
-    /*debug_message( temperature, 1 );
-    debug_message( " deg. C, H = " );
-    debug_message( humidity, 1 );
-    debug_message( "%" );*/
+    myMessage = "T = " ;
+    myMessage += temperature;
+    myMessage +=  " deg. C, H = " ;
+    myMessage += humidity;
+    myMessage += "%\n";
+    breakpoint();
+    debug_message(myMessage.c_str());
   }
 }
 
